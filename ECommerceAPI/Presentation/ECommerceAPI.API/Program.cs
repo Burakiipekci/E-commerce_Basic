e7973b06cffb6c36767fde4a39cf239a911136eb
Burak İpekci <burakipekci@outlook.com>
2023-03-29 09:59:39 +0300

    CORS Added

--- ECommerceAPI/Presentation/ECommerceAPI.API/Program.cs
+++ ECommerceAPI/Presentation/ECommerceAPI.API/Program.cs
@@ -1,11 +1,15 @@
-using ECommerceAPI.Persistence;
+﻿using ECommerceAPI.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddPersistenceServices();
 
-builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin())); // Gelebilecek isteklere izin veriyoruz.
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin())); // Gelebilecek isteklere izin veriyoruz burada.
+
+
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("http://localhost:4200", "https://localhost:4200"))); // Hangi hostingden veya porttan gelebilecek isteklere izin veriyoruz burada. Kısaca hangi clientlerdan veya sitelerde istek gelebilir onlara izin veriyoruz aksi takdirde farklı bir istek geldiğin CORS politikası meydana gelecektir.
 
 // Add services to the container.
 
